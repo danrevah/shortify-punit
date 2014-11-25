0365bcb57c20e9c783932bb8a06c50a33a52bd42
Dan Revah <danrevah89@gmail.com>
2014-11-25 20:27:29 +0200

    Add tests library

---
+++ tests/ShortifyPunitTest.php
@@ -0,0 +1,15 @@
+<?php
+// @todo Switch to bootstrap autoloader!
+require_once dirname(dirname(__FILE__)).'/src/ShortifyPunit.php';
+use Exception;
+use ShortifyPunit\ShortifyPunit;
+
+class ShortifyPunitTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInstanceOfMock()
+    {
+        $exceptionMock = ShortifyPunit::mock('Exception');
+
+        $this->assertInstanceOf('Exception', $exceptionMock);
+    }
+}
