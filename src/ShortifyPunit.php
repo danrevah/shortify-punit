152d2227569773262b5e0720fc990701ecb2627d
Dan Revah <danrevah89@gmail.com>
2014-11-29 01:46:10 +0200

    Code quality adjustments

--- src/ShortifyPunit.php
+++ src/ShortifyPunit.php
@@ -7,9 +7,11 @@ use ShortifyPunit\Enums\MockAction;
 use ShortifyPunit\Exceptions\ExceptionFactory;
 use ShortifyPunit\Stub\WhenCase;
 use ShortifyPunit\Stub\WhenChainCase;
+use ShortifyPunit\Mock\MockClass;
 
 class ShortifyPunit
 {
+    use MockClass;
     use ArgumentMatcher;
     use ExceptionFactory;
 
@@ -110,7 +112,6 @@ class ShortifyPunit
         $extends = $reflection->isInterface() ? 'implements' : 'extends';
         $marker = $reflection->isInterface() ? ", {$namespace}\\Mock\\MockInterface" : "implements {$namespace}\\Mock\\MockInterface";
 
-
         $namespaceDeclaration = $mockedNamespace ? "namespace $mockedNamespace;" : '';
         $mockerClass = $mockedNamespace.'\\'.$mockedObjectName;
 
@@ -119,73 +120,7 @@ class ShortifyPunit
             return new $mockerClass();
         }
 
-        $class =<<<EOT
-  $namespaceDeclaration
-  class $mockedObjectName $extends $className $marker {
-   public \$mockInstanceId;
-
-   public function __construct() {
-        \$this->mockInstanceId = {$namespace}\\{$basename}::generateInstanceId();
-   }
-
-EOT;
-
-        /* Mocking methods */
-        foreach ($methods as $method)
-        {
-            // Ignoring final & private methods
-            if ($method->isFinal() || $method->isPrivate()) {
-                continue;
-            }
-
-            // Ignoring constructor (created earlier)
-            if ($method->name == '__construct') {
-                continue;
-            }
-
-
-            $methodName = $method->getName();
-            $returnsByReference = $method->returnsReference() ? '&' : '';
-
-            $methodParams = [];
-
-            // Get method parameters
-            foreach ($method->getParameters() as $param)
-            {
-                // Get type hinting
-                if ($param->isArray()) {
-                    $type = 'array ';
-                } else if ($param->getClass()) {
-                    $type = '\\'.$param->getClass()->getName();
-                } else {
-                    $type = '';
-                }
-
-                // Get default value if exists
-                try {
-                    $paramDefaultValue = $param->getDefaultValue();
-                } catch (\ReflectionException $e) {
-                    $paramDefaultValue = NULL;
-                }
-
-                // Changing the params into php function definition
-                $methodParams[] = $type . ($param->isPassedByReference() ? '&' : '') .
-                                   '$'.$param->getName() . ($param->isOptional() ? '=' . var_export($paramDefaultValue, 1) : '');
-            }
-
-            $methodParams = implode(',', $methodParams);
-
-
-            $class .=<<<EOT
-    public function $returnsByReference $methodName ({$methodParams}) {
-        return {$namespace}\\{$basename}::__create_response('{$mockedObjectName}', \$this->mockInstanceId, '{$methodName}', func_get_args());
-    }
-EOT;
-
-        }
-
-        $class .= '}';
-
+        $class = static::mockClass($namespaceDeclaration, $mockedObjectName, $extends, $className, $marker, $namespace, $basename, $methods);
 
         eval($class);
 
