cc7e1762d7fe9d6ddb55233620841f491b1e4c77
Dan Revah <danrevah89@gmail.com>
2014-11-29 23:31:18 +0200

    Revert "Code Quality"
    This reverts commit bc1d70a66ee7fa365cdb32782cc6603e245aa515.

--- src/Mock/MockClass.php
+++ src/Mock/MockClass.php
@@ -120,10 +120,6 @@ class MockClass
         /* Mocking methods */
         foreach ($methods as $method)
         {
-            if ( ! $method instanceof \ReflectionMethod) {
-                continue;
-            }
-
             // Ignoring final & private methods
             if ($method->isFinal() || $method->isPrivate()) {
                 continue;
@@ -137,7 +133,33 @@ class MockClass
             $methodName = $method->getName();
             $returnsByReference = $method->returnsReference() ? '&' : '';
 
-            $methodParams = self::extractMethodParameters($method);
+            $methodParams = [];
+
+            // Get method parameters
+            foreach ($method->getParameters() as $param)
+            {
+                // Get type hinting
+                if ($param->isArray()) {
+                    $type = 'array ';
+                } else if ($param->getClass()) {
+                    $type = '\\' . $param->getClass()->getName();
+                } else {
+                    $type = '';
+                }
+
+                // Get default value if exists
+                try {
+                    $paramDefaultValue = $param->getDefaultValue();
+                } catch (\ReflectionException $e) {
+                    $paramDefaultValue = NULL;
+                }
+
+                // Changing the params into php function definition
+                $methodParams[] = $type . ($param->isPassedByReference() ? '&' : '') .
+                    '$' . $param->getName() . ($param->isOptional() ? '=' . var_export($paramDefaultValue, true) : '');
+            }
+
+            $methodParams = implode(',', $methodParams);
 
 
             $class .= <<<EOT
@@ -271,41 +293,6 @@ EOT;
         $action = $response['action'];
         $value = $response['value'];
         return array($action, $value);
-    }
-
-    /**
-     * @param \ReflectionMethod $method
-     * @return array|string
-     */
-    protected static function extractMethodParameters(\ReflectionMethod $method)
-    {
-        $methodParams = [];
-
-        // Get method parameters
-        foreach ($method->getParameters() as $param) {
-            // Get type hinting
-            if ($param->isArray()) {
-                $type = 'array ';
-            } else if ($param->getClass()) {
-                $type = '\\' . $param->getClass()->getName();
-            } else {
-                $type = '';
-            }
-
-            // Get default value if exists
-            try {
-                $paramDefaultValue = $param->getDefaultValue();
-            } catch (\ReflectionException $e) {
-                $paramDefaultValue = NULL;
-            }
-
-            // Changing the params into php function definition
-            $methodParams[] = $type . ($param->isPassedByReference() ? '&' : '') .
-                '$' . $param->getName() . ($param->isOptional() ? '=' . var_export($paramDefaultValue, true) : '');
-        }
-
-        $methodParams = implode(',', $methodParams);
-        return $methodParams;
     }
 
 } 
