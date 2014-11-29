b564b77e02ae596f94e92925abdd3ee7773c533f
Dan Revah <danrevah89@gmail.com>
2014-11-29 23:44:12 +0200

    Added mock trait

---
+++ src/Mock/MockTrait.php
@@ -0,0 +1,124 @@
+<?php
+namespace ShortifyPunit\Mock;
+
+
+trait MockTrait
+{
+
+    /**
+     * @param $methods
+     * @param $namespace
+     * @param $basename
+     * @param $mockedObjectName
+     * @param $class
+     * @return string
+     */
+    protected static function mockClassMethods($methods, $namespace, $basename, $mockedObjectName, $class)
+    {
+        /* Mocking methods */
+        foreach ($methods as $method)
+        {
+            /** @var $method \ReflectionMethod */
+            // Ignoring final & private methods
+            if ($method->isFinal() || $method->isPrivate()) {
+                continue;
+            }
+
+            // Ignoring constructor (created earlier)
+            if ($method->name == '__construct') {
+                continue;
+            }
+
+            $methodName = $method->getName();
+            $returnsByReference = $method->returnsReference() ? '&' : '';
+
+            $methodParams = [];
+
+            // Get method parameters
+            foreach ($method->getParameters() as $param)
+            {
+                // Get type hinting
+                if ($param->isArray()) {
+                    $type = 'array ';
+                } else if ($param->getClass()) {
+                    $type = '\\' . $param->getClass()->getName();
+                } else {
+                    $type = '';
+                }
+
+                // Get default value if exists
+                try {
+                    $paramDefaultValue = $param->getDefaultValue();
+                } catch (\ReflectionException $e) {
+                    $paramDefaultValue = NULL;
+                }
+
+                // Changing the params into php function definition
+                $methodParams[] = $type . ($param->isPassedByReference() ? '&' : '') .
+                    '$' . $param->getName() . ($param->isOptional() ? '=' . var_export($paramDefaultValue, true) : '');
+            }
+
+            $methodParams = implode(',', $methodParams);
+
+
+            $class .= <<<EOT
+    public function $returnsByReference $methodName ({$methodParams}) {
+        return {$namespace}\\{$basename}::_create_response('{$mockedObjectName}', \$this->mockInstanceId, '{$methodName}', func_get_args());
+    }
+EOT;
+
+        }
+        return $class;
+    }
+
+
+    /**
+     * @param \ReflectionClass $reflection
+     * @param $namespace
+     * @param $basename
+     * @return mixed
+     */
+    protected static function mockClass(\ReflectionClass $reflection, $namespace, $basename)
+    {
+        $mockedObjectName = $reflection->getShortName().'Mock';
+
+        $className = $reflection->getName();
+        $methods = $reflection->getMethods();
+
+        if ($reflection->isInterface()) {
+            $extends = 'implements';
+            $marker = ", {$namespace}\\Mock\\MockInterface" ;
+        } else {
+            $extends = 'extends';
+            $marker = "implements {$namespace}\\Mock\\MockInterface";
+        }
+
+        $mockedNamespace = $reflection->getNamespaceName();
+        $namespaceDeclaration = $mockedNamespace ? "namespace $mockedNamespace;" : '';
+
+        $mockerClass = "{$mockedNamespace}\\{$mockedObjectName}";
+
+        // Prevent duplicate mocking, return new instance of the mocked class
+        if (class_exists($mockerClass, FALSE)) {
+            return new $mockerClass();
+        }
+
+        $class = <<<EOT
+  $namespaceDeclaration
+  class $mockedObjectName $extends $className $marker {
+   public \$mockInstanceId;
+
+   public function __construct() {
+        \$this->mockInstanceId = {$namespace}\\{$basename}::generateInstanceId();
+   }
+
+EOT;
+        $class = self::mockClassMethods($methods, $namespace, $basename, $mockedObjectName, $class);
+        $class .= '}';
+
+        eval($class);
+
+        return new $mockedObjectName();
+    }
+
+} 
