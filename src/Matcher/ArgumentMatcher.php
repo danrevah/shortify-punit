af9ead2a6ee763f3bc6329e050c9f5f750d4c82e
Dan Revah <danrevah89@gmail.com>
2014-12-09 12:11:03 +0200

    Argument matcher re-placed

---
+++ src/Matcher/ArgumentMatcher.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace ShortifyPunit\Matcher;
+
+use Hamcrest\AssertionError;
+
+trait ArgumentMatcher
+{
+
+    /**
+     * Checking if there is a matching Hamcrest Function
+     * in case of founding returns the Hamcrest object otherwise returns NULL
+     *
+     * @param $returnValues - Current return values hierarchy with the current method name
+     * @param $arguments - Arguments to match
+     *
+     * @return NULL or Hamcrest
+     */
+    protected static function checkMatchingArguments($returnValues, $arguments)
+    {
+        // if doesn't have exactly the arguments check for Hamcrest-PHP functions to validate
+        foreach ($returnValues as $currentMethodArguments => $currentMethod)
+        {
+            // if not its not an Hamcrest Function
+            if (strpos($currentMethodArguments, 'Hamcrest\\') === false) {
+                continue;
+            }
+
+            $hamcrest = unserialize($currentMethodArguments);
+
+            try
+            {
+                // Loop both hamcrest and arguments
+                foreach ($arguments as $index => $arg)
+                {
+                    if ( ! array_key_exists($index, $hamcrest)) {
+                        throw new AssertionError('not enough hamcrest indexes');
+                    }
+
+                    // @throws Assertion error on failure
+                    assertThat($arg, $hamcrest[$index]);
+                }
+            }
+            catch(AssertionError $e) {
+                continue;
+            }
+
+            // if didn't cached assert error then its matching an hamcrest
+            return $currentMethodArguments;
+        }
+
+        return NULL;
+    }
+}
